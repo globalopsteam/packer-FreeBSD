57a5af5636e7bb87108ece6bdb36c42fb94cf614
Bretton <bretton@hivemind.net>
2022-03-13 17:34:52 +0200

    Replacing single quotes with doublequotes and escaping inside quotes and dollar signs

--- scripts/update.sh
+++ scripts/update.sh
@@ -9,7 +9,7 @@ freebsd-update --not-running-from-cron fetch install || true
 
 # Switch to latest package
 mkdir -p /usr/local/etc/pkg/repos
-echo 'FreeBSD: { url: "pkg+http://pkg.FreeBSD.org/${ABI}/latest" }' > /usr/local/etc/pkg/repos/FreeBSD.conf
+echo "FreeBSD: { url: \"pkg+http://pkg.FreeBSD.org/\${ABI}/latest\" }" > /usr/local/etc/pkg/repos/FreeBSD.conf
 
 # Bootstrap pkg
 env ASSUME_ALWAYS_YES=yes pkg bootstrap -f
